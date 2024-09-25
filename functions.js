5c782597cd2eb0d7adc8fade6997904cd387e3cf
Jehad Ghadie <ghad1033@algonquinlive.com>
2024-09-24 23:19:28 -0400

    Completes the assignment

---
+++ functions.js
@@ -0,0 +1,53 @@
+function list(clients) {
+
+    return clients.map(client => 
+  
+      `<li class="list-group-item d-flex justify-content-between" data-index="${client.index}">
+  
+        ${client.name}
+  
+        <strong>$ ${client.balance.toFixed(2)}</strong>
+  
+      </li>`).join('');
+  
+  }
+  
+  
+  function order(clients, property) {
+  
+    return clients.sort((a, b) => {
+  
+      if (typeof a[property] === 'string') {
+  
+        return a[property].localeCompare(b[property]);
+  
+      } else {
+  
+        return a[property] - b[property];
+  
+      }
+  
+    });
+  
+  }
+  
+  
+  function total(clients) {
+  
+    return clients.reduce((acc, client) => acc + client.balance, 0);
+  
+  }
+  
+  
+  function info(index) {
+  
+    return clients.find(client => client.index === index);
+  
+  }
+  
+  
+  function search(query) {
+  
+    return clients.filter(client => client.name.toLowerCase().includes(query.toLowerCase()));
+  
+  }
